4b37937afa5b8cb2d6dd619b863d0d1a65992df6
masaya <mt23304012@ga.ttc.ac.jp>
2024-02-06 16:37:11 +0900

    create directory

---
+++ src/main/java/DTO/TopThreadDTO.java
@@ -0,0 +1,40 @@
+package DTO;
+
+import java.io.Serializable;
+
+public class TopThreadDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private int Thread_ID;
+	private String Thread_Name;
+	private String Creator_Name;
+	
+	public TopThreadDTO() {}
+
+	public int getThread_ID() {
+		return Thread_ID;
+	}
+
+	public void setThread_ID(int thread_ID) {
+		Thread_ID = thread_ID;
+	}
+
+	public String getThread_Name() {
+		return Thread_Name;
+	}
+
+	public void setThread_Name(String thread_Name) {
+		Thread_Name = thread_Name;
+	}
+
+	public String getCreator_Name() {
+		return Creator_Name;
+	}
+
+	public void setCreator_Name(String creator_Name) {
+		Creator_Name = creator_Name;
+	}
+	
+	
+
+	
+}
--- src/main/java/thread/CreateThreadServlet.java
+++ src/main/java/thread/CreateThreadServlet.java
@@ -1,3 +1,5 @@
+
+
 import java.io.IOException;
 import java.util.ArrayList;
 
